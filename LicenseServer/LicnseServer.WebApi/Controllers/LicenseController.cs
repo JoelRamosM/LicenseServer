5d44c50dbb2c93fcfda0e02df2eecb5438868033
joel <joel.r.mz@gmail.com>
2015-03-04 00:46:41 -0300

    Include Project

---
+++ LicenseServer/LicnseServer.WebApi/Controllers/LicenseController.cs
@@ -0,0 +1,16 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Results;
+
+
+namespace LicnseServer.WebApi.Controllers
+{
+    public class LicenseController : ApiController
+    {
+        [HttpGet]
+        public string Index()
+        {
+            return "Testando API!!!";
+        }
+    }
+}
