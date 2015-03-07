4da6deb9bbd0e0892003cf24484d1a6e7b40277f
joel <joel.r.mz@gmail.com>
2015-03-07 16:59:42 -0300

    Modal cadastro

--- LicenseServer/LicnseServer.WebApi/Scripts/ViewModel/LicenseViewModel.js
+++ LicenseServer/LicnseServer.WebApi/Scripts/ViewModel/LicenseViewModel.js
@@ -3,12 +3,18 @@ function LicensesViewModel() {
     var self = this;
     var url = location.origin;
 
-    var testArray = [new LicenseModel({ Id: 1, Produto: "Teste 1", Empresa: "Empresa 1", AppKey: "1" }),
-                     new LicenseModel({ Id: 2, Produto: "Teste 2", Empresa: "Empresa 2", AppKey: "2" }),
-                     new LicenseModel({ Id: 3, Produto: "Teste 3", Empresa: "Empresa 3", AppKey: "3" }),
-                     new LicenseModel({ Id: 4, Produto: "Teste 4", Empresa: "Empresa 4", AppKey: "4" }), ];
     this.Licenses = ko.observableArray();
     this.SelectedLicense = ko.observable();
+
+    this.novaLicense = ko.observable(new LicenseModel({}));
+    this.showNovo = ko.observable(false);
+    this.onNovo = function () {
+        self.showNovo(true);
+
+    }
+    this.closeNovo = function () {
+        self.showNovo(false);
+    }
 
     ko.postbox.subscribe("licenseSelected", function (value) {
         self.SelectedLicense(value);
@@ -34,10 +40,10 @@ function LicensesViewModel() {
 
 function LicenseModel(data) {
     var self = this;
-    this.Id = ko.observable(data.Id);
-    this.Produto = ko.observable(data.Produto);
-    this.Empresa = ko.observable(data.Empresa);
-    this.AppKey = ko.observable(data.AppKey);
+    this.Id = ko.observable(data.Id || "");
+    this.Produto = ko.observable(data.Produto || "");
+    this.Empresa = ko.observable(data.Empresa || "");
+    this.AppKey = ko.observable(data.AppKey || "");
     this.selected = ko.observable(false);
 
     this.onSelect = function () {
